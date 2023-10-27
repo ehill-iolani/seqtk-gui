1bb3d4c5bb575871eb69829371b0050447b22a5c
ehill-iolani <ehill@iolani.org>
2023-10-26 16:30:05 -1000

    added ability to download results

--- app.R
+++ app.R
@@ -6,24 +6,34 @@ library(reticulate)
 library(bslib)
 library(shinycssloaders)
 
+# Lazy need it to work
+outname <- "sample.fasta"
+
 # User input for .fasta file upload
 ui <- shinyUI(fluidPage(
-  theme = bs_theme(bootswatch = "litera"),
+  theme = bs_theme(bootswatch = "sandstone"),
   titlePanel("AIN Seqtk Fastq to Fasta"),
-  sidebarLayout(
+    sidebarLayout(
     sidebarPanel(
       fileInput("file1", "Choose .fastq file to upload",
-                accept = c(".fastq", ".fastq.gz", ".fq", ".fq.gz")),
+        accept = c(".fastq", ".fastq.gz", ".fq", ".fq.gz")),
       actionButton("convert", "CONVERT FASTQ TO FASTA")),
     mainPanel(
       withSpinner(verbatimTextOutput("results"))),
+  ),
+  tabPanel("Output FASTA",
+      fluidPage(
+        headerPanel("FASTA Output"),
+        downloadButton("downloadData", "Download")
+      )
   )
 ))
 
-# Use a shell script to run the remote_blast.sh script
-server <- function(input, output) {
+# Uses conda to run seqtk to convert .fastq to .fasta
+server <- function(input, output, session) {
   data <- eventReactive(input$convert, {
-        outname <- sub("\\..*$", ".fasta", input$file1)
+        # Makes output file have .fasta extension
+        outname <- sub(".*", "sample.fasta", input$file1)
         command <- paste("conda run -n seqtk seqtk seq -a", input$file1, ">", outname)
         system(command, intern = TRUE)
 
@@ -39,9 +49,23 @@ server <- function(input, output) {
 
         return(combo)})
 
+  fasta <- reactiveFileReader(3000,
+                              session,
+                              filePath = outname,
+                              readFunc = readLines)
+
   output$results <- renderText({
     data()$outtext
   })
+
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste0(outname)
+    },
+    content = function(file) {
+      writeLines(fasta(), file)
+    }
+  )
 }
 
 # Run the application
